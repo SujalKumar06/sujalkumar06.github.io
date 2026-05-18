37865462f6feb3e9d3d5990fec77c4057fe62d9e
SujalKumar06 <91939382+SujalKumar06@users.noreply.github.com>
2026-05-18 08:04:55 +0000

    Deploying to gh-pages from @ SujalKumar06/sujalkumar06.github.io@0b96de6b5cb2657a8dc4fa9da4cd0fd0dd68b192 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "alshedivat/al-folio",
+    "data-repo": "SujalKumar06/sujalkumar06.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
